6bdace72e889cad167def74fc2cc09fd3f2f2119
cmash93 <cmash93@gmail.com>
2022-09-07 16:34:26 -0500

    page going blank, gotta fix current functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var app = document.querySelector('.weather-app');
 var dailyTemp = document.querySelector('.temperature');
-var date = document.querySelector('.date');
+var dateOutput = document.querySelector('.date');
 var conditionOutput = document.querySelector('.condition');
 var nameOutput = document.querySelector('.city-name');
 var icon = document.querySelector('.icon');
@@ -12,7 +12,7 @@ var weeklyWind = document.querySelector('.weekly-wind');
 var weeklyHumidity = document.querySelector('.weekly-humidity');
 var form = document.getElementById('locationInput');
 var search = document.querySelector('.search-city');
-var btn = document.querySelector('.search-button');
+var btn = document.querySelector('.submit');
 var cities = document.querySelectorAll('.city');
 
 let cityInput = "London";
@@ -21,7 +21,8 @@ cities.forEach((city) => {
     city.addEventListener('click', (e) => {
         cityInput = e.target.innerHTML;
 
-        fetchWeather();
+        fetchDailyWeather();
+        // fetchWeeklyWeather();
 
         app.style.opacity = '0';
     })
@@ -35,7 +36,8 @@ form.addEventListener('submit', (e) => {
         cityInput = search.value;
     }
 
-    fetchWeather();
+    fetchDailyWeather();
+    // fetchWeeklyWeather();
 
     search.value = '';
 
@@ -57,14 +59,41 @@ function dayOfWeek(day, month, year) {
     return weekday[new Date(`${day}/${month}/${year}`).getDay()];
 };
 
-function fetchWeather() {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=92f58b8d29e5240f313467eab70b0b56&q=${cityInput}`)
-}
+function fetchDailyWeather() {
+    // fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=92f58b8d29e5240f313467eab70b0b56`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=92f58b8d29e5240f313467eab70b0b56`)
+    // fetch(`https://api.weatherapi.com/v1/current.json?key=92f58b8d29e5240f313467eab70b0b56&q=${cityInput}`)
+
 
 .then(response => response.json())
 .then(data => {
     console.log(data);
 
-    temp.InnerHTML = data.current.temp_c + '&#176;'
-    conditionOutput.innerHTML = data.current.condition.text;
+    dailyTemp.InnerHTML = data.main.temp + '&#176;'
+    conditionOutput.innerHTML = data.weather.main;
+
+    // const date = data.location.localtime;
+    // const y = parseInt(date.substr(0, 4));
+    // const d = parseInt(date.substr(5, 2));
+    // const m = parseInt(date.substr(8, 2));
+
+    // dateOutput.innerHTML = `${dayOfWeek(d, m, y)} ${d} - ${m} - ${y}`;
+
+    humidityOutput.innerHTML = data.main.humidity + '%';
+    windOutput.innerHTML = data.wind.speed+ 'mph';
+
+    console.log(data.main.temp)
+    // console.log(data.weather.description)
+    console.log(data.main.humidity)
+    console.log(data.wind.speed)
+
+    
+    
 })
+
+// .catch(() => {
+//     alert('City not found, please try again');
+//     app.style.opacity = '1';
+// })
+
+}
