1423ccf1ce642f0d2b12c0cb4f2716d30ef7d64f
cmash93 <cmash93@gmail.com>
2022-09-08 03:10:00 -0500

    made it look awesome lol

--- assets/script.js
+++ assets/script.js
@@ -59,15 +59,15 @@ function fetchDailyWeather() {
         cityName.innerHTML = cityInput;
 
         var dailyTemp = document.querySelector('.temperature');
-        dailyTemp.innerHTML = `${kToF(data.main.temp)}&#176;F`;
+        dailyTemp.innerHTML = `${kToF(data.main.temp)}&#176;`;
 
         var conditionOutput = document.querySelector('.condition');
         conditionOutput.innerHTML = data.weather[0].description;
 
         var icon = document.querySelector('.icon');
         icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        icon.style.height = '54px';
-        icon.style.width = '54ox';
+        icon.style.height = '100px';
+        icon.style.width = '80ox';
 
         var humidityOutput = document.querySelector('.humidity');
         humidityOutput.innerHTML = `${data.main.humidity}%`;
@@ -77,6 +77,102 @@ function fetchDailyWeather() {
 
         let lat = data.coord.lat;
         let lon = data.coord.lon;
+
+        const code = data.weather[0].id;
+        
+        var time = new Date()
+        var localTime = time.getTime()
+        var localOffset = time.getTimezoneOffset() * 60000
+        var utc = localTime + localOffset
+        var currentCity = utc + (1000 * data.timezone)
+        var nd = new Date(currentCity)
+        const currentTime = new Date(nd).getHours();
+        console.log(currentTime)
+
+
+        if (code == 800) {
+            if (7 <= currentTime && currentTime < 20){
+                app.style.backgroundImage = `url(./images/clear-day.jpg)`;
+            }
+            else {
+                app.style.backgroundImage = `url(./images/clear-night.webp)`  
+            }
+        }
+        else if (
+            code == 801 ||
+            code == 802 ||
+            code == 803 ||
+            code == 804 
+        ) {
+            if (7 <= currentTime && currentTime < 20){
+                app.style.backgroundImage = `url(./images/cloudy-day.jpg)`;
+            }
+            else {
+                app.style.backgroundImage = `url(./images/cloudy-night.jpg)`  
+            }
+        }
+        else if (
+            code == 200 ||
+            code == 201 ||
+            code == 202 ||
+            code == 210 ||
+            code == 211 ||
+            code == 212 ||
+            code == 221 ||
+            code == 230 ||
+            code == 231 ||
+            code == 232 ||
+            code == 781 ||
+            code == 300 ||
+            code == 301 ||
+            code == 302 ||
+            code == 310 ||
+            code == 311 ||
+            code == 312 ||
+            code == 313 ||
+            code == 314 ||
+            code == 315 ||
+            code == 500 ||
+            code == 501 ||
+            code == 502 ||
+            code == 503 ||
+            code == 504 ||
+            code == 511 ||
+            code == 520 ||
+            code == 521 ||
+            code == 522 ||
+            code == 531
+        ) {
+            if (7 <= currentTime && currentTime < 20){
+                app.style.backgroundImage = `url(./images/rainy-day.jpg)`;
+            }
+            else {
+                app.style.backgroundImage = `url(./images/rainy-night.jpg)`  
+            }
+        }
+        else if (
+            code == 600 ||
+            code == 601 ||
+            code == 602 ||
+            code == 611 ||
+            code == 612 ||
+            code == 613 ||
+            code == 615 ||
+            code == 616 ||
+            code == 620 ||
+            code == 621 ||
+            code == 622 
+        ){
+            if (7 <= currentTime && currentTime < 20){
+                app.style.backgroundImage = `url(./images/snowy-day.png)`;
+            }
+            else {
+                app.style.backgroundImage = `url(./images/snowy-night.jpg)`  
+            }
+        }
+        else {
+            app.style.backgroundImage = `url(./images/weather.webp)`;
+        };
 
         fetch(`https://api.openweathermap.org/data/2.5/uvi?appid=92f58b8d29e5240f313467eab70b0b56&lat=${lat}&lon=${lon}`)
             .then(response => response.json())
@@ -103,12 +199,10 @@ function fetchDailyWeather() {
             })   
     })
 
-    // YOU NEED TO ADD UV INDEX.
-
-.catch(() => {
-    alert('Unable to connect to Weather.com');
-    app.style.opacity = '1';
-})
+// .catch(() => {
+//     alert('Unable to connect to Weather.com');
+//     app.style.opacity = '1';
+// })
 
 }
 
